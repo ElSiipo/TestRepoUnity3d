b34a9b83fa0f0d65c812067c2abda650dcb099b2
ElSiipo <cbartelius@gmail.com>
2017-05-15 22:51:46 +0200

    Added targets, added color change on trigger hit

---
+++ FPS tutorial 1/Assets/OnBulletHit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBulletHit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//var target = GetComponent<GameObject>()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Player")
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+}
--- FPS tutorial 1/Assets/Scripts/GoalTriggerSound.cs
+++ FPS tutorial 1/Assets/Scripts/GoalTriggerSound.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GoalTriggerSound : MonoBehaviour {
@@ -20,10 +22,26 @@ public class GoalTriggerSound : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if(!audio.isPlaying)
+        if(!audio.isPlaying && ValidateAllTargetsHit())
         {
             Debug.Log("The trigger is fired");
             audio.PlayOneShot(goalClip, volume);
         }
+    }
+
+    private bool ValidateAllTargetsHit()
+    {
+        var targets = GameObject.FindGameObjectsWithTag("Target").Where(t => t.name.Equals("Target", StringComparison.InvariantCultureIgnoreCase));
+        var allObjectsHit = false;
+
+        foreach (var target in targets)
+        {
+            allObjectsHit = target.GetComponent<MeshRenderer>().material.color == Color.green;
+        }
+
+        return allObjectsHit;
+
+        //return targets.All(p => p.name == "Target" && p.GetComponent<Renderer>().material.color == Color.green);
+        //return targets.All(p => p.name == "Target" && p.GetComponent<MeshRenderer>().material.color == Color.green);
     }
 }
